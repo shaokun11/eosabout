d74036c3b4d0320cee107e695fad956ccb2bc542
shaokun11 <605416017@qq.com>
2019-01-12 12:26:10 +0800

    eos dispatcher 学习1

---
+++ addressbook/addressbook.cpp
@@ -0,0 +1,95 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract]] addressbook : public eosio::contract {
+
+public:
+  using contract::contract;
+  
+  addressbook(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+
+  [[eosio::action]]
+  void upsert(name user, std::string first_name, std::string last_name) {
+    require_auth(user);
+    address_index addresses(_code, _code.value);
+    auto iterator = addresses.find(user.value);
+    if( iterator == addresses.end() )
+    {
+      addresses.emplace(user, [&]( auto& row ) {
+       row.key = user;
+       row.first_name = first_name;
+       row.last_name = last_name;
+      });
+      send_summary(user, " successfully emplaced record to addressbook");
+    }
+    else {
+      std::string changes;
+      addresses.modify(iterator, user, [&]( auto& row ) {
+        row.key = user;
+        row.first_name = first_name;
+        row.last_name = last_name;
+      });
+      send_summary(user, " successfully modified record to addressbook");
+    }
+  }
+
+  [[eosio::action]]
+  void erase(name user) {
+    require_auth(user);
+
+    address_index addresses(_self, _code.value);
+
+    auto iterator = addresses.find(user.value);
+    eosio_assert(iterator != addresses.end(), "Record does not exist");
+    addresses.erase(iterator);
+    send_summary(user, " successfully erased record from addressbook");
+  }
+
+  [[eosio::action]]
+  void notify(name user, std::string msg) {
+    require_auth(get_self());
+    require_recipient(user);
+  }
+
+private:
+  struct [[eosio::table]] person {
+    name key;
+    std::string first_name;
+    std::string last_name;
+    uint64_t primary_key() const { return key.value; }
+  };
+
+  void send_summary(name user, std::string message) {
+    action(
+      permission_level{get_self(),"active"_n},
+      get_self(),
+      "notify"_n,
+      std::make_tuple(user, name{user}.to_string() + message)
+    ).send();
+  };
+
+  typedef eosio::multi_index<"people"_n, person> address_index;
+  
+};
+
+extern "C" {
+    void apply(uint64_t receiver, uint64_t code, uint64_t action) {
+        auto self = receiver;
+        if( code == self ) {
+          addressbook _addressbook(name(receiver),name(code),datastream<const char*>(nullptr,0));
+          switch(action) {
+            case name("upsert").value: 
+              execute_action(name(receiver), name(code), &addressbook::upsert); 
+              break;
+            case name("notify").value: 
+              execute_action(name(receiver), name(code), &addressbook::notify); 
+              break;
+            case name("erase").value: 
+              execute_action(name(receiver), name(code), &addressbook::erase); 
+              break;
+          }
+        }
+    }
+};
